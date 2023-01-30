1fc50856aae01556ff8e759488e54857c0c955de
RonaldMartinez00 <mronald16@yahoo.com>
2023-01-29 16:13:29 -0800

    test

--- controllers/user.js
+++ controllers/user.js
@@ -31,6 +31,11 @@ router.post('/',async (req, res) => {
         res.status(500).send(err.message);
     }
 });
+router.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 
