072c964d7a2135b4634e24d8c47a257b7ee578b8
Alex Aguirre <teamaguirre@gmail.com>
2023-01-30 10:39:29 -0800

    changed the server.js file

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 require('dotenv').config()
-const dotenv = require('dotenv')
 const express = require('express')
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const userRoutes = require("./controllers/user");
 const app = express();
-const port = process.env.PORT;
-const uri = process.env.MONGO_URI;
 const mealRoutes = require("./controllers/meals");
 const cors = require('cors');
-const { METHODS } = require('http');
+const defineCurrentUser = require('./routes/defineuser')
 
 
 
@@ -23,6 +19,7 @@ app.use(cors(corsOptions));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json())
 app.use(express.static('public'))
+app.use(defineCurrentUser)
 
 /*mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
@@ -35,6 +32,10 @@ app.options('*',cors());
 app.use("/", userRoutes);
 app.use("/meals",mealRoutes);
 app.use("/auth",require('./controllers/auth'))
+
+app.get('/', (req,res)=>{
+  res.send(`Server running on port ${process.env.PORT}`)
+})
 // Start the server
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server listening on port ${process.env.PORT || 5000}`);
